c3b297d898ca59c7d2ce59481e3f812d2e9736f8
yossely <yossely7@gmail.com>
2018-08-07 17:10:19 -0500

    Create unit test for local user and api user models

--- src/app/users/shared/models/api-user.model.spec.ts
+++ src/app/users/shared/models/api-user.model.spec.ts
@@ -1,8 +1,23 @@
-/* import { ApiUser } from './api-user.model';
+import { ApiUser } from './api-user.model';
 
 describe('ApiUser', () => {
+  let user: ApiUser;
+
+  beforeEach(() => {
+    user = new ApiUser({
+      id: 123,
+      first_name: 'Yossely',
+      last_name: 'Mendoza',
+      avatar: 'avatar url'
+    } as ApiUser);
+  });
+
   it('should create an instance', () => {
-    // expect(new ApiUser()).toBeTruthy();
+    expect(user).toBeTruthy();
+  });
+
+  it('should return the user full name', () => {
+    expect(user.fullName).toEqual('Yossely Mendoza');
   });
 });
- */
+
--- src/app/users/shared/models/local-user.model.spec.ts
+++ src/app/users/shared/models/local-user.model.spec.ts
@@ -1,8 +1,61 @@
-/* import { LocalUser } from './local-user.model';
+import { LocalUser } from './local-user.model';
 
 describe('LocalUser', () => {
+  let user: LocalUser;
+
+  beforeEach(() => {
+    user = new LocalUser({
+      id: '123',
+      firstName: 'Diego',
+      lastName: 'Juliao',
+      email: 'dianjuar@gmail.com',
+      phone: '123123123',
+      birthDate: new Date('08/08/1991'),
+      createdAt: new Date()
+    } as LocalUser);
+
+  });
+
   it('should create an instance', () => {
-    // expect(new LocalUser()).toBeTruthy();
+    expect(user).toBeTruthy();
+    expect(user.age).toBeDefined();
+  });
+
+  it('should return the user full name', () => {
+    expect(user.getFullName()).toEqual('Diego Juliao');
+  });
+
+  it('should set the updated at date', () => {
+    // set updated date with a string
+    const newUpdatedAtString = '07/05/2018';
+    user.setUpdatedAt(newUpdatedAtString);
+
+    expect(user.updatedAt).toEqual(new Date(newUpdatedAtString));
+
+    // set updated date with a Date
+    const newUpdatedAtDate = new Date('06/07/2018');
+    user.setUpdatedAt(newUpdatedAtDate);
+
+    expect(user.updatedAt).toEqual(newUpdatedAtDate);
+  });
+
+  it('should set the created at date', () => {
+    // set created date with a string
+    const newCreatedAtString = '01/03/2018';
+    user.setCreatedAt(newCreatedAtString);
+
+    expect(user.createdAt).toEqual(new Date(newCreatedAtString));
+
+    // set created date with a Date
+    const newCreatedAtDate = new Date('04/21/2018');
+    user.setCreatedAt(newCreatedAtDate);
+
+    expect(user.createdAt).toEqual(newCreatedAtDate);
+  });
+
+  it('should create an empty local user', () => {
+    const expectedData = new LocalUser({} as any);
+    expect(LocalUser.initEmptyUser()).toEqual(expectedData);
   });
 });
- */
+
